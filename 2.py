3457986feb92a671c71c622cedbcff31a0e7bfc3
Jayitha <jayithareddyp@gmail.com>
2019-04-20 16:51:03 +0530

    Added univariate two sample t tests with confidence intervals

---
+++ 2.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy import stats
+import math
+#X1 = np.array([])
+#X2 = np.array([])
+#n1 = X1.shape[0]
+#n2 = X2.shape[0]
+n1 = 15
+n2 = 20
+alpha = 1-0.95
+df = n1 + n2 - 2
+#x1bar = int(np.mean(X1))
+#x2bar = int(np.mean(X2))
+x1bar = 2.514
+x2bar = 2.963
+print("x1bar: ",x1bar, " x2bar: ",x2bar)
+#SS1 = n1*int(np.cov(X1))
+#SS2 = n2*int(np.cov(X2))
+#S2pl = (SS1 + SS2)/ df
+SS1 = 0
+SS2 = 0
+S2pl = 0.642**2
+print("SS1: ", SS1," SS2: ", SS2," S2pl: ",S2pl)
+t = (x1bar - x2bar)/math.sqrt(S2pl*(n1 + n2)/(n1*n2))
+print("t: ",t)
+tcritical = stats.t.ppf(1-alpha/2, df)
+print("tcritical: ", tcritical)
+if abs(t) > tcritical:
+    print("t > tcritical: Reject Hypothesis")
+elif abs(t) < tcritical:
+    print("t < tcritical: Accept Hypothesis")
+else:
+    print("t = tcritical: Marginal, choose")
+CI = tcritical*(math.sqrt(S2pl*(n1+n2)/(n1*n2)))
+print("Margin: ",CI)
+print("CI: (",(x1bar - x2bar) - CI," : ",(x1bar - x2bar) + CI,")")
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MSI END SEMESTER EXAMINATION FILES
 
-* Test for H0: mu = mu0 when sigma known
+* 1.Test for H0: mu = mu0 when sigma known
+* 2.Univariate Two Sample t test with confidence intervals 
